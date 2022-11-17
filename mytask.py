1a8b7d06d52bf232a662d1e08f650800b7b7a3ff
User <Test@FADEYI-PC.lasuth.org.ng.local>
2022-11-17 13:14:50 +0100

    changes

--- mytask.py
+++ mytask.py
@@ -1,7 +1,18 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from pydantic import BaseModel
 
 app = FastAPI()
+
+origins = ["*"]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 myDetails = {
     "name": "Abby",
