0189caab11d0b377f71f17f68f09eef3b69a42e1
User <Test@FADEYI-PC.lasuth.org.ng.local>
2022-11-17 12:55:29 +0100

    added to mytask.py

--- mytask.py
+++ mytask.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
 
 app = FastAPI()
 
@@ -6,11 +7,19 @@ myDetails = {
     "name": "Abby",
     "Age": 20,
     "track": "Backend Python",
-    "hobbies": {"watching football", "coding"}
+    "hobbies": ["watching football", "coding"]
 }
+class myDetail(BaseModel):
+    name: str
+    age: int
+    track: int
 
 @app.get("/details")
 def response():
     return myDetails
+
+@app.post("/data")
+def create_data(details = MyDetail):
+    return details
 
 
